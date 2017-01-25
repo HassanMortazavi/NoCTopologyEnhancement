ec2eea48fbc9491b36597917161978c7e98da872
HassanMortazavi <mortazavi91@gmail.com>
2017-01-25 15:06:09 +0330

    code improvement

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 from Topology import Topology
 from datetime import datetime
 
-myTopology = Topology(16, 16, 4, 4, [0, 4, 3, 2, 43, 53, 100, 254], 0)
+myTopology = Topology(16, 16, 4, 4, [0, 4, 3, 2, 43, 53, 100, 254], 0.09)
 
-print(myTopology.get_novel_distance_fault_free(0, 254))
-print(myTopology.get_novel_distance_faulty_des(0, 254))
-
+print("start ...")
 before_run_time = datetime.now()
-print(myTopology.get_topology_avg_distance())
-print(datetime.now() - before_run_time)
+print("topology average distance = " + str(myTopology.get_topology_avg_distance()))
+print("exec time = " + str(datetime.now() - before_run_time))
