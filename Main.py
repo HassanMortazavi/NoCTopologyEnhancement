aa8037d85a68c8c916791ecb2b4a2e11864df89c
HassanMortazavi <mortazavi91@gmail.com>
2017-01-25 16:58:07 +0330

    code improvement

--- Main.py
+++ Main.py
@@ -1,16 +1,11 @@
-from Topology import Topology
+from SA import SA
 from datetime import datetime
 
 
-def get_hub_connections_list(network_size):
-    result = []
-    for i in range(0, network_size):
-        result.append(i % 2)
-    return result
 
-
-myTopology = Topology(16, 16, 4, 4, get_hub_connections_list(256), 0.99)
 print("start ...")
 before_run_time = datetime.now()
-print("topology average distance = " + str(myTopology.get_topology_avg_distance()))
+
+mySA = SA(256,0.9)
+
 print("exec time = " + str(datetime.now() - before_run_time))
